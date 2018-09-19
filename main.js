97d514dc20f37d5041aee049d2eceaf699844745
Max Bachinsky <maxbachinsky99@gmail.com>
2018-09-19 02:00:25 -0400

    The first commit of the extension, written in javascript

---
+++ main.js
@@ -0,0 +1,11 @@
+javascript: var highlight=window.getSelection().toString();
+var i =0;
+var count = 1;
+var character;
+for (i = 0; i < highlight.length; i++){
+  if (highlight.substring(i,i+1).localeCompare(" ")==0){
+    count++;
+  }
+
+}
+alert("The word count is: "+ count\n"The character count is: "+highlight.length);
