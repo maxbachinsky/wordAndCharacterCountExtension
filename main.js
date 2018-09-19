d263eecb42c0e383efaf2b29f99cf962b596904a
Max Bachinsky <maxbachinsky99@gmail.com>
2018-09-19 03:40:44 -0400

    put into an unpacked chrome extension

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
-javascript: var highlight=window.getSelection().toString();
+var highlight=window.getSelection().toString();
 var i =0;
 var count = 1;
-var character;
 for (i = 0; i < highlight.length; i++){
   if (highlight.substring(i,i+1).localeCompare(" ")==0){
     count++;
   }
 
 }
-alert("The word count is: "+ count+".\nThe character count is: "+highlight.length+".");
+alert("The word count is: "+ count+".\n" +"The character count is: "+highlight.length+".");
